723b02a09ed1d9395130ea179d68b4b1a6779bf5
Julien Pactat <julien.pactat.akyos@gmail.com>
2023-07-29 19:38:18 +0200

    Before refacto

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from 'next/image'
+"use client";
+
+import { getSession, signOut } from 'next-auth/react'
+import { NextPageContext } from 'next'
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+}
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-2xl text-green-500'>Netflix</h1>
+      <h1 className='text-4xl text-green-500'>Netflix</h1>
+      <button className='w-full h-10 bg-white' onClick={() => signOut()}>Se déconnecter</button>
     </>
   )
 }
